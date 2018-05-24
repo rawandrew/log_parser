b9e772f35956821a58b9ccf8b6a2c812111e10ca
Andrei Crudu <andrei.crudu@yahoo.com>
2018-05-24 15:36:07 +0300

    Add the intializer to the stats class

--- bin/stats.rb
+++ bin/stats.rb
@@ -1,3 +1,9 @@
-class Stats
+require_relative 'log_line_parser'
 
+class Stats
+  attr_reader :line_parser
+
+  def initialize(line_parser: LogLineParser.new)
+    @line_parser = line_parser
+  end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 require_relative '../bin/stats'
+require_relative '../bin/log_line_parser'
 
 RSpec.describe Stats do
 
   it 'can instantiate a Stats class' do
     expect(Stats.new).not_to be nil
+  end
+
+  it 'can intialize the stats with a log line parser' do
+    stats = Stats.new(line_parser: LogLineParser.new)
+    expect(stats.line_parser).to be_a LogLineParser
+  end
+
+  it 'contains a default log line parser if one isn\'t passed in' do
+    stats = Stats.new
+    expect(stats.line_parser).to be_a LogLineParser
   end
 end
