a1616100b38e000f113f869c230d1ef33799d4ec
Andrei Crudu <andrei.crudu@yahoo.com>
2018-05-24 21:37:04 +0300

    Finish moving all terminology fom stats to log analyzer

--- parser.rb
+++ parser.rb
@@ -2,8 +2,8 @@
 require_relative 'bin/log_analyzer'
 
 file = File.open(ARGV.first)
-stats = LogAnalyzer.new file: file
+analyzer = LogAnalyzer.new file: file
 puts 'Webpages ordered by page views descending:'
-puts stats.generate_page_views_stats
+puts analyzer.generate_page_views_stats
 puts 'Webpages ordered by unique page views descending:'
-puts stats.generate_unique_page_views_stats
+puts analyzer.generate_unique_page_views_stats
--- spec/log_analyzer_spec.rb
+++ spec/log_analyzer_spec.rb
@@ -5,28 +5,28 @@ require_relative '../bin/log_line_parser'
 
 RSpec.describe LogAnalyzer do
 
-  it 'can instantiate a Stats class' do
+  it 'can instantiate a log analyzer class' do
     expect(LogAnalyzer.new).not_to be nil
   end
 
-  it 'can intialize the stats with a log line parser' do
-    stats = LogAnalyzer.new(line_parser: LogLineParser.new)
-    expect(stats.line_parser).to be_a LogLineParser
+  it 'can intialize the LogAnalyzer with a log line parser' do
+    analyzer = LogAnalyzer.new(line_parser: LogLineParser.new)
+    expect(analyzer.line_parser).to be_a LogLineParser
   end
 
   it 'contains a default log line parser if one isn\'t passed in' do
-    stats = LogAnalyzer.new
-    expect(stats.line_parser).to be_a LogLineParser
+    analyzer = LogAnalyzer.new
+    expect(analyzer.line_parser).to be_a LogLineParser
   end
 
-  it 'can initialize the stats with a file object' do
-    stats = LogAnalyzer.new(file: File.open(File.join(Dir.pwd, 'webserver.log'), 'r'))
-    expect(stats.file).to be_a File
+  it 'can initialize the log analyzer with a file object' do
+    analyzer = LogAnalyzer.new(file: File.open(File.join(Dir.pwd, 'webserver.log'), 'r'))
+    expect(analyzer.file).to be_a File
   end
 
   it 'contains false instead of a file if one isn\'t passed in' do
-    stats = LogAnalyzer.new
-    expect(stats.file).to be false
+    analyzer = LogAnalyzer.new
+    expect(analyzer.file).to be false
   end
 
   it 'intializes the data after reading the file' do
@@ -39,15 +39,15 @@ RSpec.describe LogAnalyzer do
           "/contact" => { visits: 2, visitors: Set.new(['184.123.665.067'])},
           "/home" => { visits: 1, visitors: Set.new(['184.123.665.068'])}
       }
-      stats = LogAnalyzer.new file: f
-      expect(stats.data).to eq expected_result
+      analyzer = LogAnalyzer.new file: f
+      expect(analyzer.data).to eq expected_result
     end
   end
 
   context '#get_most_page_views' do
     it 'returns "No log file to process" if no file is avaiable' do
-      stats = LogAnalyzer.new
-      expect(stats.generate_page_views_stats).to eq 'No log file to process'
+      analyzer = LogAnalyzer.new
+      expect(analyzer.generate_page_views_stats).to eq 'No log file to process'
     end
 
     it 'returns the website pages ordered by number of page views' do
@@ -56,16 +56,16 @@ RSpec.describe LogAnalyzer do
         f.puts '/contact 184.123.665.067'
         f.puts '/home 184.123.665.068'
         f.rewind
-        stats = LogAnalyzer.new(file: f)
-        expect(stats.generate_page_views_stats).to eq "/contact 2 visits\n/home 1 visits"
+        analyzer = LogAnalyzer.new(file: f)
+        expect(analyzer.generate_page_views_stats).to eq "/contact 2 visits\n/home 1 visits"
       end
     end
   end
 
   context '#get_most_unique_page_views' do
     it 'returns "No log file to process" if no file is avaiable' do
-      stats = LogAnalyzer.new
-      expect(stats.generate_unique_page_views_stats).to eq 'No log file to process'
+      analyzer = LogAnalyzer.new
+      expect(analyzer.generate_unique_page_views_stats).to eq 'No log file to process'
     end
 
     it 'returns the website pages ordered by number of page views' do
@@ -75,8 +75,8 @@ RSpec.describe LogAnalyzer do
         f.puts '/home 184.123.665.068'
         f.puts '/home 184.123.665.067'
         f.rewind
-        stats = LogAnalyzer.new(file: f)
-        expect(stats.generate_unique_page_views_stats).to eq "/home 2 visits\n/contact 1 visits"
+        analyzer = LogAnalyzer.new(file: f)
+        expect(analyzer.generate_unique_page_views_stats).to eq "/home 2 visits\n/contact 1 visits"
       end
     end
   end
