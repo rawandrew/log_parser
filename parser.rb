bbc8c585dc2b6ea95114e0e8442d523645ad4a76
Andrei Crudu <andrei.crudu@yahoo.com>
2018-05-24 21:20:49 +0300

    Make it possible to run the script from the terminal

--- parser.rb
+++ parser.rb
@@ -1,1 +1,9 @@
 #!/usr/bin/env ruby
+require_relative 'bin/stats'
+
+file = File.open(File.join(Dir.pwd, ARGV.first), 'r')
+stats = Stats.new file: file
+puts 'Webpages ordered by most views:'
+puts stats.get_most_page_views
+puts 'Webpages ordered by most unique views:'
+puts stats.get_most_unique_page_views
