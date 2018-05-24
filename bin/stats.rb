e8e90f248e0e7861411707363a13159ad74456fa
Andrei Crudu <andrei.crudu@yahoo.com>
2018-05-24 16:11:08 +0300

    Make it possible to intialize the stats with a file

--- bin/stats.rb
+++ bin/stats.rb
@@ -1,9 +1,18 @@
 require_relative 'log_line_parser'
 
 class Stats
-  attr_reader :line_parser
+  attr_reader :line_parser, :file
 
-  def initialize(line_parser: LogLineParser.new)
+  def initialize(file: false, line_parser: LogLineParser.new)
     @line_parser = line_parser
+    @file = file
+  end
+
+  def get_most_page_views
+
+  end
+
+  def get_most_unique_page_views
+
   end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -17,4 +17,22 @@ RSpec.describe Stats do
     stats = Stats.new
     expect(stats.line_parser).to be_a LogLineParser
   end
+
+  it 'can intialize the stats with a file object' do
+    stats = Stats.new file: File.open(File.join(Dir.pwd, 'webserver.log'), 'r')
+    expect(stats.file).to be_a File
+  end
+
+  it 'contains false instead of a file if one isn\'t passed in' do
+    stats = Stats.new
+    expect(stats.file).to be false
+  end
+
+  context '#get_most_page_views' do
+
+  end
+
+  context '#get_most_unique_page_views' do
+
+  end
 end
